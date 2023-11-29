997d5b624fd6d72958b2b81736a37e6a61341fc6
sanchezcarlosjr <24639141+sanchezcarlosjr@users.noreply.github.com>
2023-11-29 14:08:57 -0800

    feat: listen selection changes

--- src/evatutor/service.py
+++ src/evatutor/service.py
@@ -1,52 +1,15 @@
 import json
-from threading import Thread
-
+from langchain.chat_models import ChatOpenAI
+from langchain.schema import AIMessage, HumanMessage
 import gradio as gr
-import torch
-from transformers import AutoModelForCausalLM, AutoTokenizer, StoppingCriteria, StoppingCriteriaList, \
-    TextIteratorStreamer
-
-tokenizer = AutoTokenizer.from_pretrained("togethercomputer/RedPajama-INCITE-Chat-3B-v1")
-model = AutoModelForCausalLM.from_pretrained("togethercomputer/RedPajama-INCITE-Chat-3B-v1", torch_dtype=torch.float16)
-model = model.to('cuda:0')
-
-
-class StopOnTokens(StoppingCriteria):
-    def __call__(self, input_ids: torch.LongTensor, scores: torch.FloatTensor, **kwargs) -> bool:
-        stop_ids = [29, 0]
-        for stop_id in stop_ids:
-            if input_ids[0][-1] == stop_id:
-                return True
-        return False
 
 
 def bot(history_transformer_format):
     history_transformer_format[-1][1] = ""
-    stop = StopOnTokens()
-
-    messages = "".join(["".join(["\n<human>:" + item[0], "\n<bot>:" + item[1]])  # curr_system_message +
-                        for item in history_transformer_format])
-
-    model_inputs = tokenizer([messages], return_tensors="pt").to("cuda")
-    streamer = TextIteratorStreamer(tokenizer, timeout=10., skip_prompt=True, skip_special_tokens=True)
-    generate_kwargs = dict(
-        model_inputs,
-        streamer=streamer,
-        max_new_tokens=1024,
-        do_sample=True,
-        top_p=0.95,
-        top_k=1000,
-        temperature=1.0,
-        num_beams=1,
-        stopping_criteria=StoppingCriteriaList([stop])
-    )
-    t = Thread(target=model.generate, kwargs=generate_kwargs)
-    t.start()
-
-    for new_token in streamer:
-        if new_token != '<':
-            history_transformer_format[-1][1] += new_token
-            yield history_transformer_format
+    response = "Cool!"
+    for new_token in response:
+        history_transformer_format[-1][1] += new_token
+        yield history_transformer_format
 
 
 def load_json(path):
@@ -56,10 +19,19 @@ def load_json(path):
 
 prompts = load_json("./prompts.json")
 
+llm = ChatOpenAI(temperature=1.0, model='gpt-3.5-turbo-0613')
 
-def add_text(history, text, choice):
-    history = history + [(text, None)]
-    return history, gr.Textbox(value="", interactive=False)
+
+def predict(history, choice, user_prompt, user_selection):
+    history_langchain_format = []
+    for human, ai in history:
+        history_langchain_format.append(HumanMessage(content=human))
+        history_langchain_format.append(AIMessage(content=ai))
+    history = history + [[prompts[choice][1] + "\n Usuario: " + user_prompt + "\n Codigo/Pregunta: " + user_selection, ""]]
+    history_langchain_format.append(HumanMessage(content=history[-1][0]))
+    gpt_response = llm(history_langchain_format)
+    history[-1][1] = gpt_response.content
+    return history
 
 
 def add_file(history, file):
@@ -74,20 +46,29 @@ def vote(data: gr.LikeData):
         print("You downvoted this response: " + data.value)
 
 
-with gr.Blocks(title="Evatutor", css="footer{display:none !important}") as demo:
+with gr.Blocks(title="Evatutor", css="footer{display:none !important} .chatbot {bottom: 12em!important;}", js="""(() => {
+document.addEventListener("selectionchange", () => {
+  const currentUserSelection = document.getSelection().toString();
+  if (currentUserSelection) {
+  document.getElementById("evatutor_user_selection").children[0].children[1].value = currentUserSelection;
+    document.getElementById("evatutor_user_selection").children[0].children[1]
+         .dispatchEvent(new Event('input', { detail: { value: currentUserSelection } }));
+  }
+});
+})""") as demo:
     chatbot = gr.Chatbot([], elem_id="chatbot", bubble_full_width=True, show_label=False)
 
-    choice = gr.Dropdown(list(map(lambda prompt: prompt[0], prompts)), type="index", label="Agent Context",
-                         info="Modify the context of your agent to enhance its helpfulness.")
+    choice = gr.Dropdown(list(map(lambda prompt: prompt[0], prompts)), type="index", label="Contexto del agente",
+                         info="Busca el agente que mejor se adapte a tus dudas.")
 
-    txt = gr.Textbox(scale=4, show_label=False, placeholder="Enter text and press enter", container=False)
+    user_selection = gr.Textbox(label="Tu duda es sobre:", visible=False, interactive=False, elem_id='evatutor_user_selection')
 
-    txt_msg = txt.submit(add_text, [chatbot, txt, choice], [chatbot, txt], queue=False).then(bot, chatbot, chatbot,
-                                                                                             api_name="bot_response")
-    txt_msg.then(lambda: gr.Textbox(interactive=True), None, [txt], queue=False)
+    user_prompt = gr.Textbox(label="Tu mensaje:")
+
+    btn = gr.Button(value="Preguntar")
+    btn.click(predict, inputs=[chatbot, choice, user_prompt, user_selection], outputs=[chatbot])
 
     chatbot.like(vote, None, None)
-
 
 demo.queue()
 demo.launch()
