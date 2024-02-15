98a2effaed2d6cd30fa00d40c30aa0e2253dc3e5
sanchezcarlosjr <24639141+sanchezcarlosjr@users.noreply.github.com>
2024-02-14 20:05:32 -0800

    feat: improve user interface

--- evanote-plugin/src/gradio-float-chatbot.ts
+++ evanote-plugin/src/gradio-float-chatbot.ts
@@ -1,6 +1,5 @@
-import { LitElement, css, html } from 'lit'
-import { customElement, property } from 'lit/decorators.js'
-import avatar from './avatar.png';
+import {css, html, LitElement} from 'lit'
+import {customElement, property} from 'lit/decorators.js'
 import 'https://gradio.s3-us-west-2.amazonaws.com/4.15.0/gradio.js';
 
 /**
@@ -9,60 +8,122 @@ import 'https://gradio.s3-us-west-2.amazonaws.com/4.15.0/gradio.js';
  */
 @customElement('gradio-float-chatbot')
 export class GradioFloatChatbot extends LitElement {
-  @property({ type: Boolean })
-  showChatBot = false;
+    @property({type: Boolean}) showChatBot = false;
 
-  render() {
-    return html`
-      <img @click=${this._onClick}  src=${avatar} class="chatbot-toggle" alt="ChatBot Float" height="512" width="512" />
-      ${this.showChatBot ? html`<slot></slot>` : ""}
+    render() {
+        return html`
+            <li @click=${this._onClick} class="chatbot-toggle">EvaTutor</li>
+            ${this.showChatBot ? html`
+                <slot></slot>` : ""}
+        `
+    }
+
+    private _onClick() {
+        this.showChatBot = !this.showChatBot;
+    }
+
+    static styles = css`
+      .chatbot-toggle {
+            display: -webkit-inline-box;
+    display: -webkit-inline-flex;
+    display: -ms-inline-flexbox;
+    display: inline-flex;
+    -webkit-align-items: center;
+    -webkit-box-align: center;
+    -ms-flex-align: center;
+    align-items: center;
+    -webkit-box-pack: center;
+    -ms-flex-pack: center;
+    -webkit-justify-content: center;
+    justify-content: center;
+    position: relative;
+    box-sizing: border-box;
+    -webkit-tap-highlight-color: transparent;
+    background-color: transparent;
+    outline: 0;
+    border: 0;
+    margin: 0;
+    border-radius: 0;
+    padding: 0;
+    cursor: pointer;
+    -webkit-user-select: none;
+    -moz-user-select: none;
+    -ms-user-select: none;
+    user-select: none;
+    vertical-align: middle;
+    -moz-appearance: none;
+    -webkit-appearance: none;
+    -webkit-text-decoration: none;
+    text-decoration: none;
+    color: inherit;
+    font-weight: 400;
+    font-size: 1rem;
+    line-height: 1.5;
+    letter-spacing: 0.00938em;
+    display: -webkit-box;
+    display: -webkit-flex;
+    display: -ms-flexbox;
+    display: flex;
+    -webkit-box-pack: start;
+    -ms-flex-pack: start;
+    -webkit-justify-content: flex-start;
+    justify-content: flex-start;
+    -webkit-align-items: center;
+    -webkit-box-align: center;
+    -ms-flex-align: center;
+    align-items: center;
+    position: relative;
+    -webkit-text-decoration: none;
+    text-decoration: none;
+    min-height: 48px;
+    padding-top: 6px;
+    padding-bottom: 6px;
+    box-sizing: border-box;
+    white-space: nowrap;
+    padding-left: 16px;
+    padding-right: 16px;
+      }
+      
+      ::slotted(.chatbot) {
+        border: 0;
+        position: fixed;
+        width: 30em;
+        height: 54em;
+        top: 1.5em;
+        z-index: 2;
+        right: 0.1em;
+      }
     `
-  }
-
-  private _onClick() {
-    this.showChatBot = !this.showChatBot;
-  }
-
-  static styles = css`
-    .chatbot-toggle {
-      position: fixed;
-      bottom: 1em;
-      right: 1.5em;
-      width: 4em;
-      height: 4em;
-      background-color: #f1f1f1;
-      border-radius: 20em;
-      box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
-      overflow: hidden;
-      z-index: 3;
-      cursor: pointer;
-    }
-
-    ::slotted(.chatbot) {
-      border: 0;
-      position: fixed;
-      bottom: 0;
-      right: 3em;
-      width: 32em;
-      height: 38.5em;
-      z-index: 2;
-    }
-  `
 }
 
+const id = "cicese-evatutor-chatbot";
+
 function addElement(src: string) {
-  const evanoteFloatChatBot = document.createElement("gradio-float-chatbot");
-  const gradioApp = document.createElement("gradio-app");
-  gradioApp.setAttribute('src', src);
-  gradioApp.classList.add('chatbot');
-  evanoteFloatChatBot.appendChild(gradioApp);
-  document.body.appendChild(evanoteFloatChatBot);
+    const evanoteFloatChatBot = document.createElement("gradio-float-chatbot");
+    evanoteFloatChatBot.id = id;
+    const gradioApp = document.createElement("gradio-app");
+    gradioApp.setAttribute('src', src);
+    gradioApp.classList.add('chatbot');
+    evanoteFloatChatBot.appendChild(gradioApp);
+    const header = document.getElementById("header-flex-end");
+    header?.prepend(evanoteFloatChatBot);
+}
+
+function removeElement() {
+    const evanoteFloatChatBot = document.getElementById(id);
+    evanoteFloatChatBot?.remove();
 }
 
 declare global {
-  interface HTMLElementTagNameMap {
-    'gradio-float-chatbot': GradioFloatChatbot
-  }
+    interface HTMLElementTagNameMap {
+        'gradio-float-chatbot': GradioFloatChatbot
+    }
 }
 
-addElement("https://27dd768bce6de1b822.gradio.live");
+export function mount() {
+    addElement("https://e8015202808bbbdcb4.gradio.live");
+}
+
+export function unmount() {
+    removeElement();
+}
