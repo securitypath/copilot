2966983c589c0dce83096e41805d7b098c963733
sanchezcarlosjr <24639141+sanchezcarlosjr@users.noreply.github.com>
2024-03-21 12:06:48 -0700

    refactor: move functions to class

--- src/evatutor/service.py
+++ src/evatutor/service.py
@@ -1,18 +1,9 @@
 import json
 
-from gradio import Chatbot, Button
+import gradio as gr
+from gradio import Button
 from langchain.chat_models import ChatOpenAI
 from langchain.schema import AIMessage, HumanMessage, SystemMessage
-import gradio as gr
-import time
-
-
-def bot(history_transformer_format):
-    history_transformer_format[-1][1] = ""
-    response = "Cool!"
-    for new_token in response:
-        history_transformer_format[-1][1] += new_token
-        yield history_transformer_format
 
 
 def load_json(path):
@@ -20,46 +11,66 @@ def load_json(path):
         return json.load(file)
 
 
-prompts = load_json("./prompts.json")
+class DummyAI:
+    def __init__(self):
+        self.content = "Yes"
 
-llm = ChatOpenAI(temperature=1.0, model='gpt-4-turbo-preview')
-
-
-def predict(message, history, system_prompt, user_selection, prompt_description):
-    history_langchain_format = []
-    for human, ai in history:
-        history_langchain_format.append(HumanMessage(content=human))
-        history_langchain_format.append(AIMessage(content=ai))
-
-    history_langchain_format.append(SystemMessage(content=prompts[system_prompt][2]))
-    history_langchain_format.append(HumanMessage(content=message))
-    history_langchain_format.append(HumanMessage(content="User selection: "+user_selection))
-    gpt_response = llm(history_langchain_format)
-    return gpt_response.content
+    def __call__(self, *args, **kwargs):
+        return self
 
 
-def add_file(history, file):
-    history = history + [((file.name,), None)]
-    return history
+class Prompt:
+    TITLE = 0
+    DESCRIPTION = 1
+    PROMPT_SYSTEM = 2
 
 
-def vote(data: gr.LikeData):
-    if data.liked:
-        print("You upvoted this response: " + data.value)
-    else:
-        print("You downvoted this response: " + data.value)
+class Evatutor:
+    def __init__(self, llm, prompts):
+        self.llm = llm
+        self.prompts = prompts
+        self.default_prompt = 0
+        self.prompt_titles = list(map(lambda prompt: prompt[self.default_prompt], prompts))
+        self.default_prompt_description = self.prompts[self.default_prompt][Prompt.DESCRIPTION]
+
+    @staticmethod
+    def valid_user_message(message):
+        return "" if message is None else message
+
+    def predict(self, message, history, system_prompt_id, user_selection, prompt_description):
+        history_langchain_format = []
+        for human, ai in history:
+            history_langchain_format.append(HumanMessage(content=self.valid_user_message(human)))
+            history_langchain_format.append(AIMessage(content=ai))
+
+        history_langchain_format.append(SystemMessage(content=self.prompts[system_prompt_id][Prompt.PROMPT_SYSTEM]))
+        history_langchain_format.append(HumanMessage(content=self.valid_user_message(message)))
+        history_langchain_format.append(HumanMessage(content="User selection: " + user_selection))
+        gpt_response = self.llm(history_langchain_format)
+        return gpt_response.content
+
+    @staticmethod
+    def add_file(history, file):
+        history = history + [((file.name,), None)]
+        return history
+
+    @staticmethod
+    def vote(data):
+        if data.liked:
+            print("You upvoted this response: " + data.value)
+        else:
+            print("You downvoted this response: " + data.value)
+
+    def load_initial_message(self, system_prompt_id=0):
+        history = [SystemMessage(content=self.prompts[system_prompt_id][Prompt.PROMPT_SYSTEM])]
+        gpt_response = self.llm(history)
+        return [[None, gpt_response.content]]
+
+    def change_system_prompt(self, system_prompt_id=0):
+        return self.load_initial_message(system_prompt_id), [], None, self.prompts[system_prompt_id][Prompt.DESCRIPTION]
 
 
-def load_initial_message(system_prompt=0):
-    history_langchain_format = []
-    history_langchain_format.append(SystemMessage(content=prompts[system_prompt][2]))
-    gpt_response = llm(history_langchain_format)
-    return [[None, gpt_response.content]]
-
-
-def change_system_prompt(system_prompt=0):
-    return load_initial_message(system_prompt), [], None, prompts[system_prompt][1]
-
+evatutor = Evatutor(llm=ChatOpenAI(temperature=1.0, model='gpt-4-turbo-preview'), prompts=load_json("./prompts.json"))
 
 with gr.Blocks(css="footer{display:none !important}", js="""(() => {
           document.addEventListener("selectionchange", () => {
@@ -71,33 +82,34 @@ with gr.Blocks(css="footer{display:none !important}", js="""(() => {
             }
           });
           })""") as demo:
-    system_prompt = gr.Dropdown(list(map(lambda prompt: prompt[0], prompts)), type="index",
-                                label="¿Cómo quieres que EvaTutor se comparte?",
-                                info="Busca el agente que mejor se adapte a tus dudas.", value=0,
-                                allow_custom_value=True, render=False)
+    system_prompt_id = gr.Dropdown(evatutor.prompt_titles, type="index",
+                                   label="¿Cómo quieres que EvaTutor se comparte?",
+                                   info="Busca el agente que mejor se adapte a tus dudas.",
+                                   value=evatutor.default_prompt, allow_custom_value=True, render=False)
 
-    description = gr.Textbox(value=prompts[0][1], label="Descripción", interactive=False, render=False)
+    description = gr.Textbox(value=evatutor.default_prompt_description, label="Descripción", interactive=False,
+                             render=False)
 
-    chat_interface = gr.ChatInterface(predict, retry_btn=Button(value="🔄", variant="secondary", scale=0.1, min_width=1,
-                                                                render=False),
+    chat_interface = gr.ChatInterface(evatutor.predict,
+                                      retry_btn=Button(value="🔄", variant="secondary", scale=0.1, min_width=1,
+                                                       render=False),
                                       clear_btn=Button(value="🗑️", variant="secondary", scale=0.1, min_width=1,
                                                        render=False),
                                       undo_btn=Button(value="↩️️", variant="secondary", scale=0.1, min_width=1,
                                                       render=False), submit_btn="📨",
-                                      chatbot=gr.Chatbot(value=load_initial_message(), label="EvaTutor", scale=1,
-                                                         show_label=False, latex_delimiters=[
+                                      chatbot=gr.Chatbot(value=evatutor.load_initial_message(), label="EvaTutor",
+                                                         scale=1, show_label=False, latex_delimiters=[
                                               {"left": "$", "right": "$", "display": False}], render=False),
-                                      additional_inputs=[system_prompt,description,
+                                      additional_inputs=[system_prompt_id, description,
                                                          gr.Textbox(label="Tu duda es sobre:", visible=False,
                                                                     interactive=False,
                                                                     elem_id='evatutor_user_selection')],
                                       examples=[["¿Cómo empiezo?"], ["Ayúdame a resolver el siguiente problema: "],
                                                 ["¿Qué es lo que me pide el problema?"]])
 
-    system_prompt.change(change_system_prompt, system_prompt,
-                         [chat_interface.chatbot, chat_interface.chatbot_state, chat_interface.saved_input, description],
-                         queue=False, show_api=False)
-
+    system_prompt_id.change(evatutor.change_system_prompt, system_prompt_id,
+                            [chat_interface.chatbot, chat_interface.chatbot_state, chat_interface.saved_input,
+                             description], queue=False, show_api=False)
 
 demo.queue()
 demo.launch(share=False)
